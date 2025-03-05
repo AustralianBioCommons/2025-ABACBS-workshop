3625fa65a073cae20081c21496c7d61c388d84f9
SarahBeecroft <sarah.beecroft@uwa.edu.au>
2025-03-05 14:41:42 +0800

    Update blast_array.sh

--- exercises/jobarray/blast_array.sh
+++ exercises/jobarray/blast_array.sh
@@ -3,8 +3,6 @@
 # SLURM directives
 #
 # This is an array job with four subtasks 
-#SBATCH --reservation=UWAbioinformatics
-#SBATCH --account=courses01
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=1000M
